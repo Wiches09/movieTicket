770e0a0eff769c2fb4593c015371bc5475506ef4
Wiches09 <wachnk09@gmail.com>
2026-06-12 18:32:14 +0700

    fix api

--- frontend/app/plugins/firebase.client.ts
+++ frontend/app/plugins/firebase.client.ts
@@ -3,14 +3,8 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 export default defineNuxtPlugin(() => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyB-1PAVani5xqQ0WUOcCg8GZgi2gu0vOSU",
-    authDomain: "movieticket-df802.firebaseapp.com",
-    projectId: "movieticket-df802",
-    storageBucket: "movieticket-df802.firebasestorage.app",
-    messagingSenderId: "1023879298187",
-    appId: "1:1023879298187:web:92dd45bad1c9626f8e688e",
-  };
+  const config = useRuntimeConfig();
+  const firebaseConfig = config.public.firebase;
 
   // Prevent duplicate initialization instances during hot-reloads
   const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
